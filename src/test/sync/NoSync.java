af65dc9bf811a491d2742766616f9fdf7376bf24
Den <denis100703@gmail.com>
2023-01-29 21:43:20 +0300

    Tests for thread synchronization

---
+++ src/test/sync/NoSync.java
@@ -0,0 +1,57 @@
+package test.sync;
+
+class PrintDemo {
+  public void printCount() {
+    try {
+      for (int i = 5; i > 0; i--) {
+        System.out.println("Счетчик   ---   " + i);
+      }
+    } catch (Exception e) {
+      System.out.println("Поток прерван");
+    }
+  }
+}
+
+class ThreadDemo extends Thread {
+  private Thread t;
+  private String threadName;
+  PrintDemo PD;
+
+  ThreadDemo(String name, PrintDemo pd) {
+    threadName = name;
+    PD = pd;
+  }
+
+  public void run() {
+    PD.printCount();
+    System.out.println(threadName + " завершается");
+  }
+
+  public void start() {
+    System.out.println("Запуск " + threadName);
+    if (t == null) {
+      t = new Thread(this, threadName);
+      t.start();
+    }
+  }
+}
+
+public class NoSync {
+  public static void main(String args[]) {
+    PrintDemo PD = new PrintDemo();
+
+    ThreadDemo T1 = new ThreadDemo("Поток 1", PD);
+    ThreadDemo T2 = new ThreadDemo("Поток 2", PD);
+
+    T1.start();
+    T2.start();
+
+    // ждем, пока потоки завершатся
+    try {
+      T1.join();
+      T2.join();
+    } catch (Exception e) {
+      System.out.println("Прерван");
+    }
+  }
+}
