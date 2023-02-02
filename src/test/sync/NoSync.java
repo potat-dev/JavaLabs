c49521fc3d89424525e88729abee32f77235b22d
Den <denis100703@gmail.com>
2023-02-02 11:39:20 +0300

    Remove useless files

--- src/test/sync/NoSync.java
+++ src/test/sync/NoSync.java
@@ -4,7 +4,7 @@ class PrintDemo {
   public void printCount() {
     try {
       for (int i = 5; i > 0; i--) {
-        System.out.println("Счетчик   ---   " + i);
+        System.out.println("Счетчик: " + i);
       }
     } catch (Exception e) {
       System.out.println("Поток прерван");
